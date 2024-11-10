756377d888c8c926d21b590e447b5c33cfee1fe2
mohamed fathy <mohammedfathyzaky@gmail.com>
2024-11-10 02:39:54 +0200

    final commit on the front-end

--- front-end/src/assets/script_files/helperFunc.js
+++ front-end/src/assets/script_files/helperFunc.js
@@ -24,6 +24,8 @@ function request_mapper(type,body){
 }
 
 function help_submit(formData){
+    //this functions helps the submit function to check for the validity of the input data 
+    // and returns a list of errors and where they are on faliure
     const formObject={}
     const validation_errors={}
     var toggle=true;
@@ -54,6 +56,7 @@ function check_out(state,setPage,setLoggedin){
     }
 }
 function validate_input(type,input){
+    //each input is validated based upon it's type
     switch(type.toLowerCase()){
         case 'username':
             return /^[A-Za-z][A-Za-z0-9]{4,}$/.test(input)
@@ -66,6 +69,11 @@ function validate_input(type,input){
     }
 }
 function resolve_status(result,setNotification,notification,setValidations,formData,type,setPage,setLoggedin){
+    //this function reslove the status of the answer from back end 
+    //if the log in proceeds correctly the JWT token is saved in the local storage
+    //the function switches the page to the home page
+    //the user state changes to logged in 
+    //on faluire the user is notified about the faliure and the cause of the faliure
     if(result.data.status==='success'){
         localStorage.setItem('accessToken', result.data.access_token);
         localStorage.setItem('refreshToken', result.data.refresh_token);
@@ -91,6 +99,10 @@ function resolve_status(result,setNotification,notification,setValidations,formD
     }
 }
 export function handle_submit(event,type,setNotification,notification,setValidations,setPage,setLoggedin){
+    //this formulates the form data correctly to be sent to the back end
+    //first it checks the validty of the data
+    //if correct the request is sent occurding to the form type
+    //if failed the user is notified about the wrong input field
     event.preventDefault();
     const formData = new FormData(event.target);
     const submit_validated=help_submit(formData)
@@ -115,6 +127,7 @@ export function validate_JWT(token){
 }
 
 export function log_out(setNotification,setRefill_request,setTotal,setLogged,setPage,count){
+    //a simple log out functions which deletes all the session data 
     const confirm = window.confirm('are you sure you want to log out')
     if(confirm){
         localStorage.removeItem("accessToken")
@@ -131,6 +144,11 @@ export function log_out(setNotification,setRefill_request,setTotal,setLogged,set
 }
 
 export function check_token(isMounted,setProducts,setPage,setLogged,setNotification,notification){
+    //on load the function checks the validty of the JWT  access token 
+    //if sucess the user is sent to the home page
+    //if failed the function tries a refresh from the back end
+    //if sucess the user is sent to the home page
+    //if failed the user is notified that his session has ended and asked to log on again
     const accessToken = localStorage.getItem("accessToken");
     const refreshToken = localStorage.getItem('refreshToken')
     if(validate_JWT(accessToken)){
@@ -182,22 +200,32 @@ export function ask_chart(){
 }
 
 export function try_refresh(setNotification,setRefill_request,setTotal,setCart,count,total_price,refill_request){
+    //the user is asked for confiramtion before the order is sent
+    //on confiramtion the order is sent to the back end
+    //on sucess a notification is sent to the user and the cart is emptied
+    //on faliure the user is notified to try again
     const confirm = window.confirm(`are you sure you want to submit your order`);
     if(confirm){
         const accessToken = localStorage.getItem("accessToken");
         const username = localStorage.getItem('username');
         send_order(accessToken,{'username':username,'totalprice':total_price,'orderlist':refill_request}).then((result)=>{
                 console.log(result)
+                setNotification(['Order sent',count+1])                  
+                setRefill_request({})
+                setTotal(0)
+                setCart(false)
+                sessionStorage.removeItem('refills')
+                sessionStorage.removeItem('total')
+         }).catch(()=>{
+            setNotification(['an error occuerd please try again',count+1])
          })
-         setNotification(['Order sent',count+1])
-         setRefill_request({})
-         setTotal(0)
-         setCart(false)
-         sessionStorage.removeItem('refills')
-         sessionStorage.removeItem('total')
     }
 }
 export function handel_refill(product,refill_request,total_price,setTotal,setRefill_request){
+    // the user is asked for confirmation first
+    //on  confirmation the product is added to the product list 
+    //if the order already exists it's quantity is incremnted
+    //the order list and total price is added to the session storage 
     const confirm = window.confirm(`add ${product.name} to refill list`);
         if(confirm){
             const temp={...refill_request}
